dfe7df4cb608bb7eeba49c1778003186a503cb77
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2016-11-17 19:16:00 -0600

    Initialization of the project with git

---
+++ test.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import contour_fft
+import numpy as np
+
+
+image_path = 'images/leaf1.jpg'
+show_image = True
+show_values = False
+fft = contour_fft.get_contour_fft(image_path, show_image, show_values)
+
+#freq = np.fft.fftfreq(len(valores))
+#plt.plot(freq, fft.real, freq, fft.imag)
+#plt.plot(valores)
+#print np.absolute(fft)[:10]
+plt.plot(np.absolute(fft)[:2**11])
+plt.show()
