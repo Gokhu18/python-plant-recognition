27f1a9411a6cb7df097752c4ab965692b33290aa
sjaramillo10 <severiano.jaramillo@gmail.com>
2016-12-16 12:58:03 -0600

    Added error handler in case no image path is not provided

--- calc_hist.py
+++ calc_hist.py
@@ -5,6 +5,11 @@ import cv2
 import sys
 import matplotlib.pyplot as plt
 
+if len(sys.argv) != 2:
+    print 'Error: This program should receive the path to the image!'
+    print 'Example: python2 calc_hist.py images/leaf1.jpg\n'
+    sys.exit()
+
 img = cv2.resize(cv2.imread(sys.argv[1]), (640,480)) [:,:,0]
 
 hist = cv2.calcHist([img], [0], None, [256], [0,256])
