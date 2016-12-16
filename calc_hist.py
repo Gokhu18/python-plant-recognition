f465806c22902f9ac7cbd874d336e1d197f793d3
sjaramillo10 <severiano.jaramillo@gmail.com>
2016-12-16 12:41:00 -0600

    File to calculate and show histogram of an image

---
+++ calc_hist.py
@@ -0,0 +1,14 @@
+# Recives one argument which is the route to an image then
+# calculates its histogram and plots it
+
+import cv2
+import sys
+import matplotlib.pyplot as plt
+
+img = cv2.resize(cv2.imread('images/leaf1.jpg'), (640,480)) [:,:,0]
+
+hist = cv2.calcHist([img], [0], None, [256], [0,256])
+
+plt.plot(hist)
+
+plt.show()
