10d4ce8491b797f0862aa08b4e9e66b0aeb42af9
sjaramillo10 <severiano.jaramillo@gmail.com>
2016-12-16 14:17:51 -0600

    Fixed some issues with command line arguments

--- test_thres.py
+++ test_thres.py
@@ -6,14 +6,14 @@ import sys
 if len(sys.argv) < 2 or len(sys.argv) > 4:
     print 'Error: This program should receive one to three arguments'
     print 'Usage: python2 test_thres.py "Path_to_image" "show_image" "show_values"'
-    print 'Example: python2 test_thres.py images/leaf1.jpg True False'
-    print 'Example: python2 test_thres.py images/leaf1.jpg True'
+    print 'Example: python2 test_thres.py images/leaf1.jpg 1 0'
+    print 'Example: python2 test_thres.py images/leaf1.jpg 0'
     print 'Example: python2 test_thres.py images/leaf1.jpg\n'
     sys.exit()
 
 image_path = sys.argv[1]
-show_image = False if len(sys.argv) < 3 else bool(sys.argv[2])
-show_values = False if len(sys.argv) < 4 else bool(sys.argv[3])
+show_image = False if len(sys.argv) < 3 else int(sys.argv[2])
+show_values = False if len(sys.argv) < 4 else int(sys.argv[3])
 fft = contour_fft.get_contour_fft(image_path, show_image, show_values)
 
 #freq = np.fft.fftfreq(len(valores))
