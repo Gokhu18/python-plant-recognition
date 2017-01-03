c8ae403ff58b8aa1a9e21d50c3e48c504dce5599
sjaramillo10 <severiano.jaramillo@gmail.com>
2017-01-03 14:18:34 -0600

    Fixed comments and improved histogram calc

--- calc_hist.py
+++ calc_hist.py
@@ -5,14 +5,17 @@ import cv2
 import sys
 import matplotlib.pyplot as plt
 
+# Verify correct number of command line arguments
 if len(sys.argv) != 2:
     print 'Error: This program should receive the path to the image!'
-    print 'Example: python2 calc_hist.py images/leaf1.jpg\n'
+    print 'Example: python2 calc_hist.py database/fresa_1.jpg\n'
     sys.exit()
 
-img = cv2.resize(cv2.imread(sys.argv[1]), (640,480)) [:,:,0]
+# Loads image to img
+img = cv2.imread(sys.argv[1])
 
-hist = cv2.calcHist([img], [0], None, [256], [0,256])
+# Calculates histogram of img channel 2 "blue", 256 bins
+hist = cv2.calcHist([img], [2], None, [256], [0,256])
 
 plt.plot(hist)
 
