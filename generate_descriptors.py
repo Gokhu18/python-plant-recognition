d3fbfe8073cd6eb437ef58f1075c178374af70e2
sjaramillo10 <severiano.jaramillo@gmail.com>
2017-02-14 11:35:54 -0600

    Added calculation of FFT average on each species

--- generate_descriptors.py
+++ generate_descriptors.py
@@ -18,15 +18,18 @@ name = ''
 descriptors = {}
 
 print('Calculating FFTs...\n')
-
 for image in images:
     name = image[:image.index('_')]
     if name not in descriptors:
         print(name)
         descriptors[name] = list()
 
-    fft = list(contour_fft.get_contour_fft(folder+'/'+image))
+    fft = contour_fft.get_contour_fft(folder+'/'+image)
     descriptors[name].append(fft)
+
+print('\nCalculating FFT average of each species...\n')
+for specie,descriptor in descriptors.items():
+    descriptors[specie] = list(np.mean(descriptor, axis=0))
 
 with open(output_file, 'w') as f:
     f.write(json.dumps(descriptors))
