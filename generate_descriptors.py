90a6d9e1e1d896565628199f00b6b2bf67c4d7f4
sjaramillo10 <severiano.jaramillo@gmail.com>
2017-02-14 11:13:21 -0600

    Changed some statements to improve readability

--- generate_descriptors.py
+++ generate_descriptors.py
@@ -5,28 +5,28 @@
 import matplotlib.pyplot as plt
 import contour_fft
 import numpy as np
+import json
 import os
-import re
-
-print('Reading files...\n')
 
 folder = 'database'
+output_file = 'descriptors.txt'
 
+print('Reading files...\n')
 images = sorted(os.listdir(folder))
 
 name = ''
-ffts = []
 descriptors = {}
 
 print('Calculating FFTs...\n')
 
 for image in images:
     name = image[:image.index('_')]
-    number = re.findall(r'\d+', image)[0]
-    fft = contour_fft.get_contour_fft(folder+'/'+image)
     if name not in descriptors:
         print(name)
         descriptors[name] = list()
-    descriptors[name].append(fft[:10])
 
-print descriptors
+    fft = list(contour_fft.get_contour_fft(folder+'/'+image))
+    descriptors[name].append(fft)
+
+with open(output_file, 'w') as f:
+    f.write(json.dumps(descriptors))
