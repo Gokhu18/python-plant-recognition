47253fcccb59d5c86e71f8775674fac17cd148c8
sjaramillo10 <severiano.jaramillo@gmail.com>
2017-03-02 12:00:23 -0600

    Added two versions of gray scale images

--- rgb_to_gray.py
+++ rgb_to_gray.py
@@ -1,10 +1,19 @@
 import cv2
 
-img_path = 'database/fresa.23.jpg'
+img_path = 'database/rabano.1.jpg'
 
-imgrgb = cv2.imread(img_path)
+img_rgb = cv2.imread(img_path)
 
 cv2.namedWindow('image')
-cv2.imshow('image', imgrgb)
+cv2.imshow('image', img_rgb)
 cv2.waitKey(0)
+
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+cv2.imshow('image', img_gray)
+cv2.waitKey(0)
+
+img_gray2 = img_rgb[:,:,0]
+cv2.imshow('image', img_gray2)
+cv2.waitKey(0)
+
 cv2.destroyAllWindows()
